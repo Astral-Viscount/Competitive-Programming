4d25e3012d96d0ea38042d41ba6ddca050543051
Mahi <md.mahatabmahimn@gmail.com>
2025-03-01 17:58:48 +1300

    Solved Perimeters from NZOI

--- NZOI/NZIC Practice Problems/Perimeters.py
+++ NZOI/NZIC Practice Problems/Perimeters.py
@@ -1,2 +1,17 @@
 # https://train.nzoi.org.nz/problems/777
 
+x_cord = input()
+y_cord = input()
+
+cords = list(map(int, x_cord.split() + y_cord.split()))
+
+"""
+Or,
+cords = [int(x) for x in x_cord.split()]
+cords.extend(int(y) for y in y_cord.split())
+"""
+
+vertical = cords[-1] - cords[1]
+horizontal = cords[-2] - cords[0]
+
+print((vertical * 2) + (horizontal * 2))
