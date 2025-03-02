0aac76939ee4294c1ac16910f119cce46df47ba7
Mahi <md.mahatabmahimn@gmail.com>
2025-03-02 17:32:59 +1300

    Solved Hailstone Sequences

---
+++ NZOI/NZIC Practice Problems/Hailstone-Sequences.py
@@ -0,0 +1,24 @@
+# https://train.nzoi.org.nz/problems/19
+
+numbers = []
+steps_num = []
+
+num = int(input())
+
+while num != 0:
+    numbers.append(num)
+    num = int(input())
+
+for number in numbers:
+    steps = 0
+    while number != 1:
+        if number %2 == 0:
+            number /= 2
+            steps += 1           
+        else:
+            number = (3 * number) + 1
+            steps += 1
+    steps_num.append(steps)
+
+for nums in steps_num:
+    print(nums)
