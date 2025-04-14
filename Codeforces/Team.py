309484a5e44ede354ea46fe77b5d1c8a6b7aa873
Astral <md.mahatabmahimn@gmail.com>
2025-04-15 00:28:02 +1200

    Solved Team from Codeforces Problemset

---
+++ Codeforces/Team.py
@@ -0,0 +1,11 @@
+# https://codeforces.com/problemset/problem/231/A
+
+n = int(input())
+
+count = 0
+
+for _ in range(n):
+    if sum(list(map(int, input().split()))) >= 2:
+        count += 1
+
+print(count)
