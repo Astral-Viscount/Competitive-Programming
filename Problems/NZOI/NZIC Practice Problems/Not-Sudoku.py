147a5806108b0dc0948f0142cfa2aec59e650ea4
Astral-Viscount <md.mahatabmahimn@gmail.com>
2025-07-05 23:32:31 +1200

    Solved Not-Sudoku from NZIC Practice Problems

---
+++ Problems/NZOI/NZIC Practice Problems/Not-Sudoku.py
@@ -0,0 +1,48 @@
+# https://train.nzoi.org.nz/problems/799
+
+def missing_num(nums, r):
+    total = 0
+    for i in range(1, r + 1):
+        total += i
+
+    num = total - sum(nums)
+    return num
+
+r, c = map(int, input().split())
+
+nums = []
+
+grid = []
+
+for _ in range(r):
+    axis = list(input().split())
+
+    if "x" in axis:
+        axis[axis.index('x')] = '0'
+    arr = [int(i) for i in axis]
+    grid.append(arr)
+
+index1 = None
+index2 = None
+
+for row, column in enumerate(grid):
+    if 0 in column:
+        index1 = row
+        index2 = column.index(0)
+        break
+
+for i in grid:
+    value = i[index2]
+    if value != 0:
+        value = int(value)
+        nums.append(value)
+
+
+num = missing_num(nums, r)
+
+grid[index1][index2] = num
+
+for ro in grid:
+    for col in ro:
+        print(col, end=" ")
+    print()
