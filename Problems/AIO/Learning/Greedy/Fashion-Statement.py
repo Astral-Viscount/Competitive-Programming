63031d03a3905ca7a90a8ae1523efec92a103ab8
Astral-Viscount <md.mahatabmahimn@gmail.com>
2025-08-13 18:20:37 +1200

    first AIO problem

--- Problems/AIO/Learning/Greedy/Fashion-Statement.py
+++ Problems/AIO/Learning/Greedy/Fashion-Statement.py
@@ -1,2 +1,21 @@
 # https://orac.amt.edu.au/problem/121/
 
+t = int(input())
+
+notes = 0
+
+while t != 0:
+    if t >= 100:
+        t -= 100
+        notes += 1
+    elif t >= 20:
+        t -= 20
+        notes += 1
+    elif t >= 5:
+        t -= 5
+        notes += 1
+    elif t >= 1:
+        t -= 1
+        notes += 1
+    
+print(notes)
