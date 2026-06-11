11564aba9f6dab3a1c2aec4d4e79de52e590a6c3
Astral-Viscount <md.mahatabmahimn@gmail.com>
2026-06-12 01:09:29 +1200

    cspc 2025 quick look - a,b,c

---
+++ Problems/Prog4Fun/CSPC (Canterbury Schools Programming Contests)/CSPC-2025/A--Welcome.py
@@ -0,0 +1,3 @@
+# 
+
+print("Thank You UC!")
---
+++ Problems/Prog4Fun/CSPC (Canterbury Schools Programming Contests)/CSPC-2025/C-Past-my-bedtime.py
@@ -0,0 +1,9 @@
+#
+
+time = int(input())
+
+if time > 21:
+    print("It's past your bedtime Sia!")
+else:
+
+    print(f"You have {21 - time} hours left before bed Sia!")
