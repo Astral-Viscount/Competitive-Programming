ca14b6853508db7236796f32560dfba61bc78352
Astral-Viscount <md.mahatabmahimn@gmail.com>
2026-07-23 00:38:40 +1200

    1047 easy leetcode

---
+++ Problems/Leetcode/Easy/1047.remove-all-adjacent-duplicates-in-string.py
@@ -0,0 +1,20 @@
+#
+# @lc app=leetcode id=1047 lang=python3
+#
+# [1047] Remove All Adjacent Duplicates In String
+#
+
+# @lc code=start
+class Solution:
+    def removeDuplicates(self, s: str) -> str:
+        stack = []
+
+        for char in s:
+            if stack and char == stack[-1]:
+                stack.pop()
+            else:
+                stack.append(char)
+        
+        return("".join(stack))
+# @lc code=end
+
