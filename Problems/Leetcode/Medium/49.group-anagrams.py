c39f13c62568ce5fbbd3521eb3b0052656df5050
Astral-Viscount <md.mahatabmahimn@gmail.com>
2026-07-24 14:35:35 +1200

    medium leetcode 49

---
+++ Problems/Leetcode/Medium/49.group-anagrams.py
@@ -0,0 +1,22 @@
+#
+# @lc app=leetcode id=49 lang=python3
+#
+# [49] Group Anagrams
+#
+
+# @lc code=start
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        anagrams = {}
+
+        for string in strs:
+            sorted_string = "".join(sorted(string))
+
+            if sorted_string not in anagrams:
+                anagrams[sorted_string] = []
+            
+            anagrams[sorted_string].append(string)
+        
+        return list(anagrams.values())
+# @lc code=end
+
