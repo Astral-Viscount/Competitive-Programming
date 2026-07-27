0715ae6a2978781c0a2d110c5a9b5f4d4a869b1a
Astral-Viscount <md.mahatabmahimn@gmail.com>
2026-07-27 15:13:36 +1200

    easy 844 leetcode with O(1) space complexity thx to AlgoMonster

--- Problems/Leetcode/Easy/844.backspace-string-compare.py
+++ Problems/Leetcode/Easy/844.backspace-string-compare.py
@@ -7,26 +7,61 @@
 # @lc code=start
 class Solution:
     def backspaceCompare(self, s: str, t: str) -> bool:
-        s_new = []
-        t_new = []
+        i, j = len(s) - 1, len(t) - 1
+        skip_s, skip_t = 0, 0
 
-        for char in s:
-            if char == "#":
-                if s_new:
-                    s_new.pop()
-                continue
-
-            s_new.append(char)
+        while i >= 0 and j >= 0:
+            while i >= 0:
+                if s[i] == "#":
+                    skip_s += 1
+                    i -= 1
+                elif skip_s > 0:
+                    skip_s -= 1
+                    i -= 1
+                else:
+                    break
+            
+            while j >= 0:
+                if t[j] == "#":
+                    skip_t += 1
+                    j -= 1
+                elif skip_t > 0:
+                    skip_t += 1
+                    j -= 1
+                else:
+                    break
+            
+            if i >= 0 and j >= 0:
+                if s[i] != t[j]:
+                    return False
+            elif i >= 0 or j >= 0:
+                return False
+            
+            i -= 1
+            j -= 1
         
-        for char in t:
-            if char == "#":
-                if t_new:
-                    t_new.pop()
-                continue
+        return True
 
-            t_new.append(char)
+        # s_new = []
+        # t_new = []
+
+        # for char in s:
+        #     if char == "#":
+        #         if s_new:
+        #             s_new.pop()
+        #         continue
+
+        #     s_new.append(char)
         
-        return s_new == t_new
+        # for char in t:
+        #     if char == "#":
+        #         if t_new:
+        #             t_new.pop()
+        #         continue
+
+        #     t_new.append(char)
+        
+        # return s_new == t_new
         
         # if len(s_new) != len(t_new):
         #     return False
