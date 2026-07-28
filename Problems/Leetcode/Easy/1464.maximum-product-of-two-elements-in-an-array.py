61eb48b25dca032ff88179e8eef5b255e5927675
Astral-Viscount <md.mahatabmahimn@gmail.com>
2026-07-28 19:53:00 +1200

    easy 1464 leetcode without modifying the input array

--- Problems/Leetcode/Easy/1464.maximum-product-of-two-elements-in-an-array.py
+++ Problems/Leetcode/Easy/1464.maximum-product-of-two-elements-in-an-array.py
@@ -7,7 +7,18 @@
 # @lc code=start
 class Solution:
     def maxProduct(self, nums: List[int]) -> int:
-        return (nums.pop(nums.index(max(nums))) - 1) * (nums.pop(nums.index(max(nums))) - 1)
+        big_1, big_2 = 0, 0
+
+        for num in nums:
+            if num > big_1:
+                big_2 = big_1
+                big_1 = num
+            elif num > big_2:
+                big_2 = num
+        
+        return (big1 - 1) * (big_2 - 1)
+
+        # return (nums.pop(nums.index(max(nums))) - 1) * (nums.pop(nums.index(max(nums))) - 1)
 
         # nums.sort()
         # return (nums[-1] - 1) * (nums[-2] - 1)
