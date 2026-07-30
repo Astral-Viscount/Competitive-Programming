48d93dbb5945ab18628f762f7e4021ebd06913e8
Astral-Viscount <md.mahatabmahimn@gmail.com>
2026-07-31 11:35:52 +1200

    3248 easy leetcode

---
+++ Problems/Leetcode/Easy/3248.snake-in-matrix.py
@@ -0,0 +1,24 @@
+#
+# @lc app=leetcode id=3248 lang=python3
+#
+# [3248] Snake in Matrix
+#
+
+# @lc code=start
+class Solution:
+    def finalPositionOfSnake(self, n: int, commands: List[str]) -> int:
+        row, col = 0, 0
+
+        for command in commands:
+            if command == "RIGHT":
+                col += 1
+            elif command == "LEFT":
+                col -= 1
+            elif command == "UP":
+                row -= 1
+            else:
+                row += 1
+        
+        return ((row * n) + col)
+# @lc code=end
+
